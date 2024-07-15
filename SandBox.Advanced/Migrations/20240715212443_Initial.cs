ae4fb1c25c0be457fedaccc152579b32fa7829ca
TheCraztWolf <tcrazywolf63@gmail.com>
2024-07-16 01:26:12 +0400

    Migrations

---
+++ SandBox.Advanced/Migrations/20240715212443_Initial.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SandBox.Advanced.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    IdTelegram = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    DateTimeJoined = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastActivity = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsNeedToVerifyByCaptcha = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsSpamer = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsAprroved = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsManagerThisBot = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.IdTelegram);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BlackWords",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlackWords", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChatTg",
+                columns: table => new
+                {
+                    IdChat = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatTg", x => x.IdChat);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Captchas",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IdTelegram = table.Column<long>(type: "INTEGER", nullable: true),
+                    DateTimeExpired = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    AttemptsRemain = table.Column<byte>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Captchas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Captchas_Accounts_IdTelegram",
+                        column: x => x.IdTelegram,
+                        principalTable: "Accounts",
+                        principalColumn: "IdTelegram");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ChatId = table.Column<long>(type: "INTEGER", nullable: true),
+                    DateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IdTelegram = table.Column<long>(type: "INTEGER", nullable: true),
+                    Discriminator = table.Column<string>(type: "TEXT", maxLength: 13, nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: true),
+                    IsSpam = table.Column<bool>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_Accounts_IdTelegram",
+                        column: x => x.IdTelegram,
+                        principalTable: "Accounts",
+                        principalColumn: "IdTelegram");
+                    table.ForeignKey(
+                        name: "FK_Events_ChatTg_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "ChatTg",
+                        principalColumn: "IdChat");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Captchas_IdTelegram",
+                table: "Captchas",
+                column: "IdTelegram");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ChatId",
+                table: "Events",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_IdTelegram",
+                table: "Events",
+                column: "IdTelegram");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlackWords");
+
+            migrationBuilder.DropTable(
+                name: "Captchas");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "ChatTg");
+        }
+    }
+}
--- SandBox.Models/Blackbox/Captcha.cs
+++ SandBox.Models/Blackbox/Captcha.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using SandBox.Models.Common;
+using SandBox.Models.Telegram;
 
 namespace SandBox.Models.Blackbox;
 
 public class Captcha : Entity
 {
     public long? IdTelegram { get; set; }
+    [ForeignKey("IdTelegram")]public Account? Account { get; set; }
     public DateTime DateTimeExpired { get; set; }
     public string Content { get; set; }
     public byte AttemptsRemain { get; set; }
