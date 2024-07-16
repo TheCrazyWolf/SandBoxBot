7478524ec074df8d1b0e357ce73fc4a60ea5aa33
TheCraztWolf <tcrazywolf63@gmail.com>
2024-07-16 22:59:45 +0400

    Implement add admin

--- SandBox.Advanced/Executable/Commands/SetMeManager.cs
+++ SandBox.Advanced/Executable/Commands/SetMeManager.cs
@@ -1,42 +1,88 @@
+using System.Net.Mime;
 using SandBox.Advanced.Abstract;
 using SandBox.Advanced.Database;
+using SandBox.Advanced.Services.Text;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
 namespace SandBox.Advanced.Executable.Commands;
 
-public class SetMeManager: IExecutable<bool>
+public class SetMeManager : IExecutable<bool>
 {
     public ITelegramBotClient BotClient = default!;
     public Update Update = default!;
     public SandBoxRepository Repository = default!;
     public string Secret = default!;
-    
+
     public Task<bool> Execute()
     {
         if (Update.Message?.From is null)
             return Task.FromResult(false);
-        
-        SendMessage(BuildMessage());
-        return Task.FromResult(true);
+
+        var treatmentText = TextTreatment.GetMessageWithoutUserNameBotsAndCommands(Update.Message.Text!).Split(' ')
+            .First();
+
+        if (string.IsNullOrEmpty(treatmentText))
+        {
+            SendMessageUnsuccess();
+            return Task.FromResult(true);
+        }
+
+        if (treatmentText == Secret)
+        {
+            Proccess();
+            SendMessageSuccess();
+            return Task.FromResult(true);
+        }
+
+        SendMessageUnsuccess();
+        return Task.FromResult(false);
     }
-    private Task SendMessage(string message)
+
+    private Task Proccess()
     {
-        BotClient.SendTextMessageAsync(chatId:Update.Message!.Chat.Id,
+        var profile = Repository.Accounts.GetById(Update.Message!.From!.Id).Result;
+
+        if (profile is null)
+            return Task.CompletedTask;
+
+        profile.IsAprroved = true;
+        profile.IsManagerThisBot = true;
+
+        Repository.Accounts.Update(profile);
+        return Task.CompletedTask;
+    }
+
+    private Task SendMessageSuccess()
+    {
+        var message = BuildSuccessMessage();
+
+        BotClient.SendTextMessageAsync(chatId: Update.Message!.Chat.Id,
             text: message,
             disableNotification: true);
         return Task.CompletedTask;
     }
 
-    private string BuildMessage()
+    private Task SendMessageUnsuccess()
     {
-        string version = "1.5.0";
+        var message = BuildErrorMessage();
 
-        return
-            $"\ud83c\udfaf Анти-подработка (Антиспам)\n\nВерсия: {version}" +
-            $"\n\nРазработано @kulagin_alex, by samgk.ru \n\nАнтиспам реагирует на контекст сообщений на основе машинного обучения, " +
-            $"поведению пользователей в чате. Возможны ошибочные распознавания." +
-            $"\n\nЕсли Вы столкнулись с ограничениями в беседе, мы об этом уже в курсе. В скором времени ограничения будут сняты";
+        BotClient.SendTextMessageAsync(chatId: Update.Message!.Chat.Id,
+            text: message,
+            disableNotification: true);
+        return Task.CompletedTask;
     }
-    
+
+    private string BuildSuccessMessage()
+    {
+        return
+            $"\u2705 Команда выполнена" +
+            $"\n\nВы теперь менеджер этого бота, на вас не действуют ограничения, вы можете управлять словарем и событиями";
+    }
+
+    private string BuildErrorMessage()
+    {
+        return
+            "\u26a0\ufe0f Секрет неверный";
+    }
 }
