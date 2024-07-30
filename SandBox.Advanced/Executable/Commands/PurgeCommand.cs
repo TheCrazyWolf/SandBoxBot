17c572bbbd32c16f6eb7c4cf973f44cb10b853a2
TheCrazyWolf <tcrazywolf63@gmail.com>
2024-07-30 12:57:39 +0400

    Добавлен пример использования команды

--- SandBox.Advanced/Executable/Commands/PurgeCommand.cs
+++ SandBox.Advanced/Executable/Commands/PurgeCommand.cs
@@ -12,23 +12,28 @@ public class PurgeCommand(SandBoxRepository repository, ITelegramBotClient botCl
 
     public override async void Execute(Message message)
     {
-        if (message.From is null || message.ReplyToMessage is null) return;
+
+        if (message.From is null || message.ReplyToMessage is null)
+        {
+            _ = TrySendMessage(message.Chat.Id, BuildUsageMessage());
+            return;
+        }
 
         var account = await repository.Accounts.GetByIdAsync(message.From.Id);
+        
         var memberInChat =
             await repository.MembersInChat.GetByIdAsync(idChat: message.Chat.Id, idTelegram: message.From.Id);
 
         if (account is null || memberInChat is null) return;
 
-        if (account.IsManagerThisBot || memberInChat.IsAdmin)
-        {
-            var messageId = await TrySendMessage(message.Chat.Id);
-            
-            if (messageId is null)
-                return;
-            
-            TryRemoveMessage(message.Chat.Id, Convert.ToInt32(messageId), message.ReplyToMessage.MessageId);
-        }
+        if (!account.IsManagerThisBot && !memberInChat.IsAdmin) return;
+        
+        var messageId = await TrySendMessage(message.Chat.Id, BuildMessage());
+
+        if (messageId is null)
+            return;
+
+        TryRemoveMessage(message.Chat.Id, Convert.ToInt32(messageId), message.ReplyToMessage.MessageId);
     }
 
     private async void TryRemoveMessage(long idChat, int fromId, int toId)
@@ -51,27 +56,33 @@ public class PurgeCommand(SandBoxRepository repository, ITelegramBotClient botCl
         }
     }
 
-    private async Task<int?> TrySendMessage(long idChat)
+    private async Task<int?> TrySendMessage(long idChat, string message)
     {
         try
         {
-           var result = await botClient.SendTextMessageAsync(chatId: idChat,
-                text: BuildMessage(),
+            var result = await botClient.SendTextMessageAsync(chatId: idChat,
+                text: message,
                 disableNotification: true);
-           return result.MessageId;
+            return result.MessageId;
         }
         catch
         {
             return null;
         }
-
-        return null;
     }
 
     private string BuildMessage()
     {
         return
             $"\u2705 Команда выполняется" +
-            $"\n\nОжидайте";
+            $"\n\n Ожидайте";
+    }
+
+    private string BuildUsageMessage()
+    {
+        return
+            $"Использование команды {Name}: \n\n" +
+            $"Выберите сообщение ДО какого момента необходимо очистить чат, \n" +
+            $"в сообщение укажите эту команду и отправьте. Требуются права админа чата";
     }
 }
