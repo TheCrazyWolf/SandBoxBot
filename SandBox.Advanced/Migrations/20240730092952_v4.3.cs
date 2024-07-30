f1eb3f18f41a61879dce42676217e7998fd9edcc
TheCrazyWolf <tcrazywolf63@gmail.com>
2024-07-30 13:30:36 +0400

    Реализована проверка если восстанавливалась сообщение, то не отправлять повторно

---
+++ SandBox.Advanced/Migrations/20240730092952_v4.3.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SandBox.Advanced.Migrations
+{
+    /// <inheritdoc />
+    public partial class v43 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRestored",
+                table: "Events",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRestored",
+                table: "Events");
+        }
+    }
+}
